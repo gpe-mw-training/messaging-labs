321bb697cd0703ad347023df8e20aeb1860ab41f
Charles Moulliard <ch007m@gmail.com>
2015-08-11 12:15:55 +0200

    Improve code and add method to create a queue and get statistics

--- 5_Managing_Monitoring/jmx/src/main/java/com/redhat/gpe/training/messaging/jmx/JMXClient.java
+++ 5_Managing_Monitoring/jmx/src/main/java/com/redhat/gpe/training/messaging/jmx/JMXClient.java
@@ -1,6 +1,7 @@
 package com.redhat.gpe.training.messaging.jmx;
 
 import org.apache.activemq.broker.jmx.BrokerViewMBean;
+import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -23,7 +24,8 @@ public class JMXClient {
     private static final Logger LOG = LoggerFactory.getLogger(JMXClient.class);
 
     private static String JMX_URI = "service:jmx:rmi:///jndi/rmi://localhost:1099/karaf-root";
-
+    private static String QUEUE_TEST = "TEST.QUEUE";
+    
     public static void main(String args[]) {
         JMXServiceURL url = null;
         try {
@@ -38,8 +40,30 @@ public class JMXClient {
             BrokerViewMBean mbean = MBeanServerInvocationHandler
                     .newProxyInstance(connection, name, BrokerViewMBean.class, true);
             
-            LOG.info("Broker ID : " + mbean.getBrokerId() + " - " + mbean.getBrokerId());
-            LOG.info("Broker Name : " + mbean.getBrokerName() + " - " + mbean.getBrokerName());
+            /* Broker Info */
+            LOG.info("Broker ID : " + mbean.getBrokerId());
+            LOG.info("Broker Name : " + mbean.getBrokerName());
+            LOG.info("Broker Version : " + mbean.getBrokerVersion());
+            LOG.info("Total Message Count : " + mbean.getTotalMessageCount());
+            
+            /* Broker Queue */
+            
+            // Create a queue and send a message
+            mbean.addQueue(QUEUE_TEST);
+            name = new ObjectName("org.apache.activemq:type=Broker,brokerName=amq,destinationType=Queue,destinationName=" + QUEUE_TEST);
+            QueueViewMBean queueMbean = MBeanServerInvocationHandler.newProxyInstance(connection, name, QueueViewMBean.class, true);
+            HashMap<String, String> headers = new HashMap<String, String>();
+            headers.put("timeToLive", Long.toString(2000));
+            headers.put("JMSDeliveryMode", Integer.toString(DeliveryMode.PERSISTENT));
+            for(int i=1; i<101; i++){
+                queueMbean.sendTextMessage(headers, "test-" + Integer.toString(i), "admin", "admin");
+            }
+
+            
+            // Check statistics
+            LOG.info("Messages enqueued on TEST.QUEUE : " + queueMbean.getEnqueueCount());
+            LOG.info("Queue Size : " + queueMbean.getQueueSize());
+            LOG.info("Memory Percent Usage : " + queueMbean.getMemoryPercentUsage() + " %");
             
         } catch (Throwable t) {
             LOG.error("JMX Issue : ", t);
